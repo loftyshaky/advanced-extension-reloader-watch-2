7bb0e1179ee0787b8cc4e054e71b4ab276df991e
loftyshaky <loftyshaky@gmail.com>
2021-08-09 20:35:26 +1000

    feat: update listen.ts

--- src/ts/listen.ts
+++ src/ts/listen.ts
@@ -3,11 +3,14 @@ export default () => {
         typeof (window as any).chrome !== 'undefined' && (window as any).chrome.runtime
             ? (window as any).chrome
             : (window as any).browser;
+    const advanced_extension_reloader_id = 'u6Pgzb39sN0';
 
-    we.runtime.onMessageExternal.addListener((msg: any): void => {
+    we.runtime.onMessageExternal.addListener((msg: any, sender: any, sendResponse: any): any => {
         const msg_str: string = msg.msg;
 
-        if (msg_str === 'reload_extension_u6Pgzb39sN0') {
+        if (msg_str === `reload_extension_${advanced_extension_reloader_id}`) {
+            sendResponse(`reload_triggered_${advanced_extension_reloader_id}`);
+
             we.runtime.reload();
         }
     });

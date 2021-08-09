38791ae52e5a12ccd700ae992b4112d522b5d283
loftyshaky <loftyshaky@gmail.com>
2021-08-09 21:03:11 +1000

    feat: create separate package.json file

--- rollup.config.js
+++ rollup.config.js
@@ -64,7 +64,7 @@ const config = {
         copy({
             targets: [
                 {
-                    src: 'package.json',
+                    src: 'json/package.json',
                     dest: 'dist',
                 },
             ],

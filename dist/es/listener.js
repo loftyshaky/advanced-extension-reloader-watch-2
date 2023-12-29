ca8bc962071997b0e1be2374c0b3737c59f76c7a
loftyshaky <loftyshaky@gmail.com>
2023-12-29 16:58:05 +1000

    fix: make Listener minified

--- dist/es/listener.js
+++ dist/es/listener.js
@@ -2,23 +2,4 @@
  * Copyright 2023
  */
 
-class Listener {
-    constructor() {
-        this.gl = self;
-        this.we = typeof this.gl.chrome !== 'undefined' && this.gl.chrome.runtime
-            ? this.gl.chrome
-            : this.gl.browser;
-        this.advanced_extension_reloader_id = 'u6Pgzb39sN0';
-        this.listen = () => {
-            this.we.runtime.onMessageExternal.addListener((msg, sender, sendResponse) => {
-                const msg_str = msg.msg;
-                if (msg_str === `reload_extension_${this.advanced_extension_reloader_id}`) {
-                    sendResponse(`reload_triggered_${this.advanced_extension_reloader_id}`);
-                    this.we.runtime.reload();
-                }
-            });
-        };
-    }
-}
-
-export { Listener as default };
+class Listener{constructor(){this.gl=self,this.we=void 0!==this.gl.chrome&&this.gl.chrome.runtime?this.gl.chrome:this.gl.browser,this.advanced_extension_reloader_id="u6Pgzb39sN0",this.listen=()=>{this.we.runtime.onMessageExternal.addListener(((msg,sender,sendResponse)=>{msg.msg===`reload_extension_${this.advanced_extension_reloader_id}`&&(sendResponse(`reload_triggered_${this.advanced_extension_reloader_id}`),this.we.runtime.reload())}))}}}export{Listener as default};

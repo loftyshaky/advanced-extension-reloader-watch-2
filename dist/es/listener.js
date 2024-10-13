7477586f9de0cdc972524b2696a3a6b6a2ff44aa
loftyshaky <loftyshaky@gmail.com>
2024-10-13 23:21:26 +1000

    feat: update listener files

--- dist/es/listener.js
+++ dist/es/listener.js
@@ -1,5 +1,1 @@
-/**
- * Copyright 2024
- */
-
-class Listener{constructor(){this.gl=self,this.we=void 0!==this.gl.chrome&&this.gl.chrome.runtime?this.gl.chrome:this.gl.browser,this.advanced_extension_reloader_id="u6Pgzb39sN0",this.listen=()=>{this.we.runtime.onMessageExternal.addListener(((msg,sender,sendResponse)=>{msg.msg===`reload_extension_${this.advanced_extension_reloader_id}`&&(sendResponse(`reload_triggered_${this.advanced_extension_reloader_id}`),this.we.runtime.reload())}))}}}export{Listener as default};
+const allowed_advanced_extension_reloader_ids=["hmhmmmajoblhmohkmfjeoamhdpodihlg","hagknokdofkmojolcpbddjfdjhnjdkae","bcpgohifjmmcoiemghdamamlkbcbgifg"];class Listener{constructor(){this.gl=self,this.we=void 0!==this.gl.chrome&&this.gl.chrome.runtime?this.gl.chrome:this.gl.browser,this.listen=()=>{this.we.runtime.onMessageExternal.addListener(((msg,sender,sendResponse)=>{const msg_str=msg.msg;allowed_advanced_extension_reloader_ids.includes(sender.id)&&("reload_extension"===msg_str?(sendResponse(!0),this.we.runtime.reload()):"open_popup"===msg_str?(sendResponse(!0),this.we.action.openPopup(void 0,(()=>{this.we.runtime.lastError}))):"check_if_popup_is_open"===msg_str&&this.we.runtime.getContexts({contextTypes:["POPUP"]},(contexts=>{sendResponse(0!==contexts.length)})))}))}}}export{Listener as default};
